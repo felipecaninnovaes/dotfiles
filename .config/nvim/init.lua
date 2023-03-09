a70ae12b64646285cda1a11ddf93e680ab424241
FelipeCN <felipecaninn14@gmail.com>
2023-03-09 15:51:40 -0300

    migration starts from neovim to LUA lang

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+require "plugins"
+require "options"
+require "keybinds"
+--require "rust_conf"
+-- Você pode usar o campo `config` do packer para chamar esses arquivos
+require "airlineOptions"
+require "indentBlankLineRainbow"
+require "autocommands"
