80dc40d74d9b28a30f03aadedc897646875f00a2
FelipeCN <felipecaninn14@gmail.com>
2024-01-25 10:57:53 -0300

    Fix neovim errors

--- nix/modules/packages/base.nix
+++ nix/modules/packages/base.nix
@@ -1,12 +1,19 @@
 {pkgs, ...}: {
   environment = {
     systemPackages = with pkgs; [
+      unzip
+      lua
+      python3
+      gcc
+      nodejs_21
       firefox
       git
       curl
       vim
+      neovim
       glib
       dbus
     ];
   };
 }
+
