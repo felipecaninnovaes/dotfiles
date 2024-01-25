baa621f23691f2ce082a2e6fb516c8e02f97295e
FelipeCN <felipecaninn14@gmail.com>
2024-01-25 19:23:32 -0300

    Fix Users autostart

--- nix/sessions/kde-samsung-book/configuration.nix
+++ nix/sessions/kde-samsung-book/configuration.nix
@@ -1,11 +1,10 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
+{ inputs
+, lib
+, config
+, pkgs
+, ...
 }: {
   # You can import other NixOS modules here
   imports = [
@@ -21,15 +20,6 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
     # Configure your nixpkgs instance
     config = {
@@ -40,18 +30,18 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
+  nix.nixPath = [ "/etc/nix/path" ];
   environment.etc =
     lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
+      (name: value: {
+        name = "nix/path/${name}";
+        value.source = value.flake;
+      })
+      config.nix.registry;
 
   nix.settings = {
     # Enable flakes and new 'nix' command
@@ -60,76 +50,25 @@
     auto-optimise-store = true;
   };
 
-  # FIXME: Add the rest of your current configuration
 
-  # TODO: Set your hostname
   networking.hostName = "nixos-vm";
 
-  # TODO: This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
-    # FIXME: Replace with your username
     felipecn = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
       initialPassword = "password";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEO8QBfxkWLCYb52IRUs6wakCwYQZhVYUh5QsXo/W01F felipecaninn14@gmail.com"
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel" "networkmanager" ];
+      extraGroups = [ "wheel" "networkmanager" ];
     };
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Forbid root login through SSH.
-      PermitRootLogin = "yes";
-      # Use keys only. Remove if you want to SSH using password (not recommended)
-      PasswordAuthentication = false;
-    };
-  };
-
- # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "felipecn";
-
- # Allow unfree packages
-#  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
--- nix/sessions/kde-vm/configuration.nix
+++ nix/sessions/kde-vm/configuration.nix
@@ -20,15 +20,6 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
     # Configure your nixpkgs instance
     config = {
@@ -75,63 +66,9 @@
       extraGroups = [ "wheel" "networkmanager" ];
     };
   };
-#   # Enable the X11 windowing system.
-#   services.xserver.enable = true;
-#
-#   # Enable the KDE Plasma Desktop Environment.
-#   services.xserver.displayManager.sddm.enable = true;
-#   services.xserver.desktopManager.plasma5.enable = true;
-#
-#   # Configure keymap in X11
-#   services.xserver = {
-#     layout = "us";
-#     xkbVariant = "";
-#   };
-#
-#   # Enable CUPS to print documents.
-#   services.printing.enable = true;
-#
-#   # Enable sound with pipewire.
-#   sound.enable = true;
-#   hardware.pulseaudio.enable = false;
-#   security.rtkit.enable = true;
-#   services.pipewire = {
-#     enable = true;
-#     alsa.enable = true;
-#     alsa.support32Bit = true;
-#     pulse.enable = true;
-#     # If you want to use JACK applications, uncomment this
-#     #jack.enable = true;
-#
-#     # use the example session manager (no others are packaged yet so this is enabled by default,
-#     # no need to redefine it in your config for now)
-#     #media-session.enable = true;
-#   };
-#
-#
-#   # This setups a SSH server. Very important if you're setting up a headless system.
-#   # Feel free to remove if you don't need it.
-#   services.openssh = {
-#     enable = true;
-#     settings = {
-#       # Forbid root login through SSH.
-#       PermitRootLogin = "yes";
-#       # Use keys only. Remove if you want to SSH using password (not recommended)
-#       PasswordAuthentication = false;
-#     };
-#   };
-#
-#   # Enable automatic login for the user.
-#   services.xserver.displayManager.autoLogin.enable = true;
-#   services.xserver.displayManager.autoLogin.user = "felipecn";
-#
-#   # Allow unfree packages
-#   #  nixpkgs.config.allowUnfree = true;
-#
-#   environment.systemPackages = with pkgs; [
-#     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-#     #  wget
-#   ];
+
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "felipecn";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
